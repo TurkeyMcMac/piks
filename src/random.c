1551bf82865e47829f144237d228207fc5f607bf
TurkeyMcMac <jwmhjwmh@gmail.com>
2019-02-02 09:36:30 -0500

    Add routines for scattering numbers across a range

--- src/random.c
+++ src/random.c
@@ -8,3 +8,26 @@ rand_t randomize(rand_t seed)
 	};
 	return rotright32(seed, seed) + rand_table[seed % 16];
 }
+
+void scatter_init(scatter_t *scat, size_t limit, size_t count)
+{
+	if (count == 0) {
+		scat->limit = 0;
+	} else {
+		scat->limit = limit;
+		scat->division = limit / count;
+	}
+	scat->i = 0;
+}
+
+bool scatter_has_next(const scatter_t *scat)
+{
+	return scat->i < scat->limit;
+}
+
+size_t scatter_next(scatter_t *scat, rand_t *seed)
+{
+	size_t next = scat->i + scat->division % next_random(seed);
+	scat->i += scat->division;
+	return next;
+}
--- src/random.h
+++ src/random.h
@@ -1,13 +1,25 @@
 #ifndef RANDOM_INCLUDED_
 #define RANDOM_INCLUDED_
 
+#include <stdbool.h>
+
 typedef uint32_t rand_t;
 
 rand_t randomize(rand_t seed);
+
+typedef struct {
+	size_t limit, division, i;
+} scatter_t;
 
 static inline rand_t next_random(rand_t *randp)
 {
 	return *randp = randomize(*randp);
 }
+
+void scatter_init(scatter_t *scat, size_t limit, size_t count);
+
+bool scatter_has_next(const scatter_t *scat);
+
+size_t scatter_next(scatter_t *scat, rand_t *seed);
 
 #endif /* RANDOM_INCLUDED_ */
